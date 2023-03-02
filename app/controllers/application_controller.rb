be88ad707e147ae57848b75324da788da08f15f1
wacawa <mikkunmitsuaki@gmail.com>
2023-03-02 17:41:32 +0900

    finish 1.3.4 exercise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "hello, world!"
+    render html: "¡hola, mundo!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
   end
 end
